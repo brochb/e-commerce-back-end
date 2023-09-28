f4e5b9d3b23482ba784b9d4de4903d0f07baba6b
Brochb <brochbaltzer@gmail.com>
2023-09-28 12:34:46 -0600

    forgot colon in error message

--- main/routes/api/product-routes.js
+++ main/routes/api/product-routes.js
@@ -116,7 +116,7 @@ router.delete('/:id', async (req, res) => {
       },
     });
     if (!productData) {
-      res.status(404).json({ 'No user with this id'});
+      res.status(404).json({ message: 'No user with this id' });
       return;
     }
     res.status(200).json(productData);
