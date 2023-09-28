05b3d3328ff42b45f1c86477a61ef62dcc11a69c
Brochb <brochbaltzer@gmail.com>
2023-09-28 11:09:47 -0600

    added npm i files, started routes in product-routes.js

--- main/models/Product.js
+++ main/models/Product.js
@@ -22,7 +22,7 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataType.DECIMAL,
+      type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
         isDecimal: true,
